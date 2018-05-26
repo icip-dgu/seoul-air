bfbc2fde2e91d0b6242bf20890cdb27c631bd529
keonwool <erply@naver.com>
2018-05-26 18:06:27 +0900

    google map showing on the home page
    todo: mongodb dependenct remove

--- src/main/java/com/api/Controller_webpage.java
+++ src/main/java/com/api/Controller_webpage.java
@@ -7,23 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 	public class Controller_webpage {
-		
+
+	Map<String, Integer> dictionary = new HashMap<String, Integer>();
+
 	@RequestMapping("/")
 	public String index() {
 	  return "homepage.html";
 	}
 
 
-
-	// @RequestMapping(value = "/api/{name}",  method = RequestMethod.GET)
- //  public ModelAndView method(@PathVariable String name) {
-	//   dictionary.put("jongro", 111123);
-	//   System.out.println(dictionary.get(name));
-	//   return new ModelAndView("redirect:" + String.format("http://openapi.seoul.go.kr:8088/535a7357446f6a32373762486c4c6c/json/ListAirQualityByDistrictService/1/5/%s/", dictionary.get(name)));
-	// }
+	@RequestMapping(value = "/api/{name}",  method = RequestMethod.GET)
+  public ModelAndView method(@PathVariable String name) {
+	  dictionary.put("jongro", 111123);
+	  System.out.println(dictionary.get(name));
+	  return new ModelAndView("redirect:" + String.format("http://openapi.seoul.go.kr:8088/535a7357446f6a32373762486c4c6c/json/ListAirQualityByDistrictService/1/5/%s/", dictionary.get(name)));
+	}
 	
 	///hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
