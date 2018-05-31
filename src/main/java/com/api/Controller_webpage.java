c079a583416cfb3dd7716910aee9bfa963c4c7c9
jay <kjh338076@naver.com>
2018-05-31 19:42:24 +0900

    add area code, modified url

--- src/main/java/com/api/Controller_webpage.java
+++ src/main/java/com/api/Controller_webpage.java
@@ -3,10 +3,7 @@ package com.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,23 +12,20 @@ import java.util.Map;
 public class Controller_webpage {
 
     Map<String, Integer> dictionary = new HashMap<String, Integer>();
+    String baseUrl = "http://openapi.seoul.go.kr:8088";
+    String authKey = "535a7357446f6a32373762486c4c6c";
+    String requestFileType = "json";
+    String serviceName = "ListAirQualityByDistrictService";
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "homepage.html";
     }
 
     //indirect
-    @RequestMapping(value = "/api/{name}", method = RequestMethod.GET)
+    @GetMapping("/api/{name}")
     public ModelAndView method(@PathVariable String name) {
-        dictionary.put("jongro", 111123);
-        System.out.println(dictionary.get(name));
-        return new ModelAndView("redirect:" + String.format("http://openapi.seoul.go.kr:8088/535a7357446f6a32373762486c4c6c/json/ListAirQualityByDistrictService/1/5/%s/", dictionary.get(name)));
+        return new ModelAndView("redirect:" + baseUrl+'/'+authKey+'/'+requestFileType+'/'+serviceName +"1/5/" );
     }
 
-    ///hello-world/path-variable/in28minutes
-    @GetMapping(path = "/hello-world/path-variable/{name}")
-    public BeanExample helloWorldPathVariable(@PathVariable String name) {
-        return new BeanExample(String.format("Hello World, %s", name));
-    }
 }
